e3eb599e3da9fee9a19db2c2edfb67d7600f8656
DragonMyth <yzhang3027@gatech.edu>
2017-11-02 22:02:43 -0400

    Initial Commit

---
+++ Assets/GlobalFlockingManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalFlockingManager : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject simpleFishPrefab;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
