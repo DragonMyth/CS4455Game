d850894d241305940b4093bccb45705165573701
Jiazheng <jsun303@gatech.edu>
2017-11-17 00:38:17 -0500

    Speed up music works

---
+++ Assets/scripts/Audio/playSound.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playSound : MonoBehaviour
+{
+
+    public AudioSource mySound;
+    //public AudioClip myClip;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            mySound.enabled = true;
+            mySound.loop = true;
+        }
+        else
+        {
+            mySound.enabled = false;
+            mySound.loop = false;
+        }
+    }
+}
