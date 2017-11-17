6dbc1a9f8600e4b39e39971cb12b9c951e418014
Hu, Binchen <bhu77@gatech.edu>
2017-11-16 23:33:33 -0500

    ? not working

--- Assets/scripts/Control/simplePlayerControl.cs
+++ Assets/scripts/Control/simplePlayerControl.cs
@@ -27,6 +27,9 @@ public class simplePlayerControl : MonoBehaviour {
 
     public float maxYAngle = 80f;
 
+	private float turnAngle = 10f;
+	private float turn = 0f;
+
     // Use this for initialization
 
     GameObject playerObj;
@@ -49,9 +52,20 @@ public class simplePlayerControl : MonoBehaviour {
 		float lh = Input.GetAxisRaw("Horizontal");
 		float lv = Input.GetAxisRaw("Vertical");
 
-        this.transform.position += (cam.transform.forward * lv * speed + cam.transform.right * lh * speed) * Time.timeScale;
+		Vector2 vec = Vector2.ClampMagnitude(new Vector2(lh, lv), 1.0f);
+
+		lh = vec.x;
+		lv = vec.y;
+
+		turn = Mathf.Lerp (turn, lh, Time.deltaTime * 5f);
+		turnAngle += Time.deltaTime * 10f;
 
         anim.SetFloat("Rate", Mathf.Abs(lh) + Mathf.Abs(lv));
+		anim.SetFloat ("Turn", turn);
+		Debug.Log (turn);
+
+		this.transform.position += (cam.transform.forward * lv * speed + cam.transform.right * lh * speed) * Time.timeScale;
+		this.transform.rotation = Quaternion.Euler (0, turnAngle * turn, 0);
 
         if (lh == 0 && lv == 0)
         {
