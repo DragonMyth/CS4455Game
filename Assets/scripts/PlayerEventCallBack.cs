953849769f718d9f94858c3d5f91e6bdba3fd77e
Charlie Binchen Hu <31354128+binchenhu@users.noreply.github.com>
2017-12-10 17:00:09 -0500

    fix null reference and animation error

---
+++ Assets/scripts/PlayerEventCallBack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEventCallBack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ThreadWater()
+    {
+        EventManager.TriggerEvent<PlayerThreadWaterEvent, Vector3> (this.transform.position);
+        // if (Time.timeSinceLevelLoad - lastThreadWater > minSwimSeparation) {
+        //     lastThreadWater = Time.timeSinceLevelLoad;
+        //     Debug.Log ("threading water");
+        //     EventManager.TriggerEvent<PlayerThreadWaterEvent, Vector3> (this.transform.position);
+        // }
+    }
+}
