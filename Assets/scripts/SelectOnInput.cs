332f332c023377e8e68d9ae5b63657aaad0b7b01
RyanZHe <zhe964@gmail.com>
2017-12-10 17:13:33 -0500

    Main Menu UI Update
    -Looping Behavior in Main Menu
    -Updated Highlight color
    -Decrease Front size for credit

--- Assets/scripts/SelectOnInput.cs
+++ Assets/scripts/SelectOnInput.cs
@@ -7,6 +7,9 @@ public class SelectOnInput : MonoBehaviour {
 
     public EventSystem eventSystem;
     public GameObject selectedObject;
+    public GameObject lastSelectedObject;
+    public GameObject top;
+    public GameObject buttom;
 
     private bool buttonSelected;
 
@@ -22,7 +25,19 @@ public class SelectOnInput : MonoBehaviour {
             eventSystem.SetSelectedGameObject(selectedObject);
             buttonSelected = true;
         }
-	}
+        if (buttonSelected)
+        {
+            if (eventSystem.currentSelectedGameObject == top && Input.GetAxisRaw("Vertical") == 1)
+            {
+                eventSystem.SetSelectedGameObject(lastSelectedObject);
+            }
+            if (eventSystem.currentSelectedGameObject == buttom && Input.GetAxisRaw("Vertical") == -1)
+            {
+                eventSystem.SetSelectedGameObject(selectedObject);
+            }
+        }
+        
+    }
 
     private void OnDisable()
     {
