542d67ddc4cd74dcb4e239b56857444c92ab1e3b
DragonMyth <yzhang3027@gatech.edu>
2017-12-10 15:25:47 -0500

    Add random play for fish animations

---
+++ Assets/scripts/Flocking/AnimationRandomPlay.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationRandomPlay : MonoBehaviour {
+
+	// Use this for initialization
+	Animator anim;
+	void Start () {
+		anim = GetComponent <Animator> ();
+
+		anim.Play (anim.GetCurrentAnimatorStateInfo (0).shortNameHash,0,Random.Range (0.0f,1.0f));
+	}
+
+}
